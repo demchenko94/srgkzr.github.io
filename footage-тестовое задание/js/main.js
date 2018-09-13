fc277098fa69558e11c7ad328e42e7cfaef8c7bd
Sergey Kozar <koz.srg@gmail.com>
2018-09-13 11:42:16 +0300

    -

---
+++ footage-тестовое задание/js/main.js
@@ -0,0 +1,240 @@
+(function($){
+
+	/*************************
+	  basicScript for all pages
+	*************************/
+	function basicScript() {
+		window.windowWidth = $(window).outerWidth();
+
+		/*----------------------
+	  	Main menu
+		-----------------------*/
+		// SmartMenus init
+	  $('#main-menu').smartmenus({
+	    markCurrentItem: true, //добавлять currentкласс к <a>элементу элемента, ссылающегося на текущий URL-адрес страницы
+	    markCurrentTree: true, // добавлять currentкласс к <a>элементам всех элементов-предков текущего элемента
+	  });
+
+		// SmartMenus mobile menu toggle button
+	  var $mainMenuState = $('#main-menu-state');
+	  if ($mainMenuState.length) {
+	    // animate mobile menu
+	    $mainMenuState.change(function(e) {
+	      var $menu = $('#main-menu');
+	      if (this.checked) {
+	        $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
+	      } else {
+	        $menu.show().slideUp(250, function() { $menu.css('display', ''); });
+	      }
+	    });
+	    // hide mobile menu beforeunload
+	    $(window).bind('beforeunload unload', function() {
+	      if ($mainMenuState[0].checked) {
+	        $mainMenuState[0].click();
+	      }
+	    });
+	  };
+
+
+		/*----------------------
+	  	Header form animate
+		-----------------------*/
+	  $(".bottom-line-toogle").on("click", function(){
+	  	var animateTime = 300;
+	  	var block = $(".header-bottom");
+
+			if(block.height() === 0){
+			  autoHeightAnimate(block, animateTime);
+			} else {
+			  block.stop().animate({ height: '0' }, animateTime);
+			}
+	  });
+
+
+	};//END basicScript
+
+
+	/*************************
+	  contentChangeView
+	*************************/
+	function contentChangeView(){
+		if ($("#sorting_view-select").length){
+			var primary = $("#primary");
+
+			$("#sorting_view-select").on('change', function() {
+
+				if ( this.value == "map" ){
+					primary.addClass("list-map-view");
+				}else if( this.value == "list" ){
+					primary.removeClass("list-map-view");
+				}
+
+			});
+
+		}
+	};//END contentChangeView
+
+
+	/*************************
+	  initContentMap
+	*************************/
+	window.initContentMap = function (){
+
+		if ($("#content-map").length){
+
+			var markersData = [
+				{
+					lat: 50.005375,
+					lng: 36.321587,
+					name: "Объект 1",
+					address:"Адрес 1",
+					type: "type1"
+				},
+				{
+					lat: 49.995395,
+					lng: 36.321879,
+					name: "Объект 2",
+					address:"Адрес 2",
+					type: "type2"
+				},
+				{
+					lat: 50.010850,
+					lng: 36.342411,
+					name: "Объект 3",
+					address:"Адрес 3",
+					type: "type3"
+				}
+			];
+
+      var icons = {
+        type1: {
+          icon: 'img/1.png'
+        },
+        type2: {
+          icon: 'img/2.png'
+        },
+        type3: {
+          icon: 'img/3.png'
+        }
+      };
+
+
+			var element = document.getElementById('content-map');
+			var options = {
+				zoom: 14,
+		    center: {lat: 50.004934, lng: 36.319111},
+			}
+
+		  var map = new google.maps.Map(element, options);
+
+
+	    infoWindow = new google.maps.InfoWindow();
+	    
+	    google.maps.event.addListener(map, "click", function() {
+	      infoWindow.close();
+	    });
+	    
+	    for (var i = 0; i < markersData.length; i++){
+	      var latLng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
+	      var name = markersData[i].name;
+	      var address = markersData[i].address;
+
+	      var type = markersData[i].type;
+	        
+	      addMarker(latLng, name, address, type);
+	    }
+
+
+			function addMarker(latLng, name, address, type) {
+			  var marker = new google.maps.Marker({
+			    position: latLng,
+			    map: map,
+			    icon: icons[type].icon,
+			    title: name
+			  });
+
+			  google.maps.event.addListener(marker, "click", function() {
+
+			    var contentString = '<div class="infowindow">' +
+			                            '<h3>' + name + '</h3>' +
+			                            '<p>' + address + '</p>' +
+			                        '</div>';
+
+			    infoWindow.setContent(contentString);
+
+			    infoWindow.open(map, marker);
+			  });
+			}
+
+		}//if
+	}//END initContentMap
+
+
+
+	/*************************
+	  formSearchContent
+	*************************/
+	function formSearchContent() {
+		if($("#form-choice").length){
+
+			var $this = $("#form-choice");
+
+				//submit
+			  $this.submit(function(event) {
+					event.preventDefault();
+
+					//sending
+					$.ajax({
+						type: $this.attr('method'),
+				  	url: $this.attr('action'),
+				  	data: $this.serialize(),
+				  	dataType: "json",
+				  	beforeSend: function(jqXHR, settings) {},
+				  	success: function(data, textStatus, jqXHR) {
+
+				  	},
+				  	error: function(jqXHR, textStatus, errorThrown) {
+				  		alert('Ошибка отправки данных. Попробуйте еще раз.');
+				  	},
+				  	complete: function(jqXHR, textStatus) {},
+				  });
+
+				});//submit
+		};//if
+	};//END formSearchContent
+
+
+
+
+	/*************************
+	  SSS
+	*************************/
+	function SSS(){
+
+	};//END SSS
+
+	/*************************
+	  other functions
+	*************************/
+	function autoHeightAnimate(element, time){
+	  var curHeight = element.height();
+	  var autoHeight = element.css('height', 'auto').height();
+	  element.height(curHeight);
+	  element.stop().animate({ height: autoHeight }, time);
+	}
+	//END other
+
+
+	$(document).ready(function(){
+		basicScript();
+		contentChangeView();
+		formSearchContent();
+	});
+
+	$(window).on( "load", function(){
+		initContentMap();
+	});
+	$(window).on( "resize", function(){});
+	$(window).on( "scroll", function() {});
+
+})(jQuery);
--- footage/js/main.js
+++ footage/js/main.js
@@ -6,204 +6,186 @@
 	function basicScript() {
 		window.windowWidth = $(window).outerWidth();
 
-		/*----------------------
-	  	Main menu
-		-----------------------*/
-		// SmartMenus init
-	  $('#main-menu').smartmenus({
-	    markCurrentItem: true, //добавлять currentкласс к <a>элементу элемента, ссылающегося на текущий URL-адрес страницы
-	    markCurrentTree: true, // добавлять currentкласс к <a>элементам всех элементов-предков текущего элемента
-	  });
 
-		// SmartMenus mobile menu toggle button
-	  var $mainMenuState = $('#main-menu-state');
-	  if ($mainMenuState.length) {
-	    // animate mobile menu
-	    $mainMenuState.change(function(e) {
-	      var $menu = $('#main-menu');
-	      if (this.checked) {
-	        $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
-	      } else {
-	        $menu.show().slideUp(250, function() { $menu.css('display', ''); });
-	      }
-	    });
-	    // hide mobile menu beforeunload
-	    $(window).bind('beforeunload unload', function() {
-	      if ($mainMenuState[0].checked) {
-	        $mainMenuState[0].click();
-	      }
-	    });
-	  };
-
-
-		/*----------------------
-	  	Header form animate
-		-----------------------*/
-	  $(".bottom-line-toogle").on("click", function(){
-	  	var animateTime = 300;
-	  	var block = $(".header-bottom");
-
-			if(block.height() === 0){
-			  autoHeightAnimate(block, animateTime);
-			} else {
-			  block.stop().animate({ height: '0' }, animateTime);
-			}
-	  });
-
-
-	};//END basicScript
-
-
-	/*************************
-	  contentChangeView
-	*************************/
-	function contentChangeView(){
-		if ($("#sorting_view-select").length){
-			var primary = $("#primary");
-
-			$("#sorting_view-select").on('change', function() {
-
-				if ( this.value == "map" ){
-					primary.addClass("list-map-view");
-				}else if( this.value == "list" ){
-					primary.removeClass("list-map-view");
-				}
-
+		/*--toogle slide panel--*/
+		if ($("#m-slide-panel").length){
+		
+			$(".m-slide-panel-toogle").on("click", function (){
+				$("#m-slide-panel").toggleClass("active");
 			});
 
 		}
-	};//END contentChangeView
+
+
+		/*--добавление липкого футера, если не IE--*/
+		if (!isIE()){
+			$("#page-wrapper").addClass("sticky-footer");
+		}
+
+	};//END basicScript
 
 
 	/*************************
 	  initContentMap
 	*************************/
 	window.initContentMap = function (){
-
 		if ($("#content-map").length){
 
-			var markersData = [
-				{
-					lat: 50.005375,
-					lng: 36.321587,
-					name: "Объект 1",
-					address:"Адрес 1",
-					type: "type1"
-				},
-				{
-					lat: 49.995395,
-					lng: 36.321879,
-					name: "Объект 2",
-					address:"Адрес 2",
-					type: "type2"
-				},
-				{
-					lat: 50.010850,
-					lng: 36.342411,
-					name: "Объект 3",
-					address:"Адрес 3",
-					type: "type3"
-				}
-			];
 
-      var icons = {
-        type1: {
-          icon: 'img/1.png'
-        },
-        type2: {
-          icon: 'img/2.png'
-        },
-        type3: {
-          icon: 'img/3.png'
-        }
-      };
-
-
-			var element = document.getElementById('content-map');
-			var options = {
-				zoom: 14,
-		    center: {lat: 50.004934, lng: 36.319111},
-			}
-
-		  var map = new google.maps.Map(element, options);
-
-
-	    infoWindow = new google.maps.InfoWindow();
-	    
-	    google.maps.event.addListener(map, "click", function() {
-	      infoWindow.close();
-	    });
-	    
-	    for (var i = 0; i < markersData.length; i++){
-	      var latLng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
-	      var name = markersData[i].name;
-	      var address = markersData[i].address;
-
-	      var type = markersData[i].type;
-	        
-	      addMarker(latLng, name, address, type);
-	    }
-
-
-			function addMarker(latLng, name, address, type) {
-			  var marker = new google.maps.Marker({
-			    position: latLng,
-			    map: map,
-			    icon: icons[type].icon,
-			    title: name
-			  });
-
-			  google.maps.event.addListener(marker, "click", function() {
-
-			    var contentString = '<div class="infowindow">' +
-			                            '<h3>' + name + '</h3>' +
-			                            '<p>' + address + '</p>' +
-			                        '</div>';
-
-			    infoWindow.setContent(contentString);
-
-			    infoWindow.open(map, marker);
-			  });
-			}
 
 		}//if
 	}//END initContentMap
 
 
+	/*************************
+	  searchFormAnimation
+	*************************/
+	function searchFormAnimation() {
+		if($("#search_form").length){
+
+			var form = $("#search_form");
+			var items = form.find(".search_list_i");
+			var items_sub = form.find(".search_list_i-sub");
+
+			var search_form_open = false; //открыто ли подменю
+
+			//клик по элементу списка
+			form.find(".search_field").on("click", function (){
+				$this = $(this);
+				if ( !$this.parent().hasClass("active") ){//если закрыто - открываем
+					
+					$this.parent().addClass("active");
+					$this.siblings().slideToggle(100);
+
+					search_form_open = true;
+
+					if ($this.hasClass("search_field-adress")){//если открыли подменю
+						search_form_open = false;
+					}
+
+				}else {//если открыто - закрываем
+
+					$this.parent().removeClass("active");
+					$this.siblings().fadeOut(100);
+
+					if ( items.filter(".active").length ){//если под меню, то оставляем search_form_open = true; чтоб закрылось основное
+						search_form_open = true;
+					}else {
+						search_form_open = false;
+					}
+
+				}//search_drop
+
+			});
+			//END клик по элементу списка//.is( ":focus" )
+
+			//клик по кнопке ок в sub menu
+			$(".search_button-ok").on("click", function (){
+					items.filter(".active").each(function(){
+						$(this).removeClass("active");
+						$(this).find(".search_drop").fadeOut(100);
+					});
+			});
+
+			//клик по документу и закрытие открытых
+			$(document).mouseup(function (e){ // событие клика по веб-документу
+
+				if ( search_form_open ){ //если открыто выпадающее меню
+
+					items.filter(".active").each(function(){
+						
+						if ( !$(this).is(e.target ) // если клик был не по нашему блоку
+						    && $(this).has(e.target).length === 0 ) { // и не по его дочерним элементам
+
+							if ( !items_sub.hasClass("active") ){
+								$(this).removeClass("active");
+								$(this).find(".search_drop").fadeOut(100);
+							}else{
+								items_sub.each(function(){
+									$(this).removeClass("active");
+									$(this).find(".search_drop").fadeOut(100);
+								})
+							}
+
+							search_form_open = false;
+						}//if target
+
+					})
+
+				}//if
+
+			});
+			//END клик по документу и закрытие открытых
+
+			//Изменяем информацию в элементе формы
+			form.find(".search_input-i").change(function(){
+				$this = $(this);
+				$form_items_html = "";
+				$drop_box = $this.parents(".search_drop");//выпадающий элемент
+				$form_list_item = $drop_box.parent();//элемент списка меню формы
+
+				$($drop_box[0]).find(".search_input-i:checked").each(function(){//набиваем строку в цикле из checked инпутов
+					$form_items_html += "<span>" + this.value + "</span>";
+				})
+
+				if ( $form_items_html ){//были ли checked инпуты
+					$form_list_item.addClass("activated");//скрыть placeholder
+					$drop_box.siblings(".search_field").find(".search_field_text").html($form_items_html);
+				}else {
+					$form_list_item.removeClass("activated");
+				}
+
+			});
+			//END Изменяем информацию в элементе формы
+
+			//autocomplete input
+			var autocomplete_input = $("#autocomplete");
+			autocomplete_input.on("click", function (){
+					if ( autocomplete_input.is( ":focus" ) ){
+						autocomplete_input.parents(".search_list_i-street").addClass("active");
+					}
+			});
+
+			autocomplete_input.focusout(function() {
+			  autocomplete_input.parents(".search_list_i-street").removeClass("active");
+
+				$drop_box = $(this).parents(".search_drop");//выпадающий элемент
+				$form_items_html = this.value;
+				
+					if ( $form_items_html ){// добавляем текст родительскому элементу формы
+						
+						setTimeout(function($form_items_html){//время чтоб в инпуте появился выбор
+							$form_items_html = this.value;
+						}, 200);
+
+						$drop_box.siblings(".search_field").find(".search_field_text").html($form_items_html);
+						$drop_box.parent().addClass("activated");//скрыть placeholder
+						$(this).parent().parent().addClass("activated");
+					}else{
+						$(this).parent().parent().removeClass("activated");
+					}
+			});
+			//END autocomplete input
+
+		}//if
+	}//END searchFormAnimation
+
 
 	/*************************
-	  formSearchContent
+	  formSearchSubmit
 	*************************/
-	function formSearchContent() {
-		if($("#form-choice").length){
+	function formSearchSubmit() {
+		if($("#search_form").length){
 
-			var $this = $("#form-choice");
+			var $this = $("#search_form");
 
 				//submit
 			  $this.submit(function(event) {
 					event.preventDefault();
 
-					//sending
-					$.ajax({
-						type: $this.attr('method'),
-				  	url: $this.attr('action'),
-				  	data: $this.serialize(),
-				  	dataType: "json",
-				  	beforeSend: function(jqXHR, settings) {},
-				  	success: function(data, textStatus, jqXHR) {
-
-				  	},
-				  	error: function(jqXHR, textStatus, errorThrown) {
-				  		alert('Ошибка отправки данных. Попробуйте еще раз.');
-				  	},
-				  	complete: function(jqXHR, textStatus) {},
-				  });
-
 				});//submit
 		};//if
-	};//END formSearchContent
-
-
+	};//END formSearchSubmit
 
 
 	/*************************
@@ -216,19 +198,32 @@
 	/*************************
 	  other functions
 	*************************/
+	// 
 	function autoHeightAnimate(element, time){
 	  var curHeight = element.height();
 	  var autoHeight = element.css('height', 'auto').height();
 	  element.height(curHeight);
 	  element.stop().animate({ height: autoHeight }, time);
 	}
-	//END other
+	//END 
+	//Поиск браузеров
+	function isIE() {
+	  var ua = window.navigator.userAgent;
+	  var msie = ua.indexOf('MSIE '); // IE 10 or older
+	  var trident = ua.indexOf('Trident/'); //IE 11
+
+	  return (msie > 0 || trident > 0);
+	}
+	//END Поиск браузеров
+
+
+	//END other functions
 
 
 	$(document).ready(function(){
 		basicScript();
-		contentChangeView();
-		formSearchContent();
+		searchFormAnimation();
+		formSearchSubmit();
 	});
 
 	$(window).on( "load", function(){
@@ -237,4 +232,8 @@
 	$(window).on( "resize", function(){});
 	$(window).on( "scroll", function() {});
 
+
+
+
 })(jQuery);
+
